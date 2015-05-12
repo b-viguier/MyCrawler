6ca8edc84bbbafe7c93a755639077db8033f3105
Benoit Viguier <benoit+github@wizacha.com>
2015-05-12 22:20:53 +0200

    Added price parsing

--- src/ProductParser.php
+++ src/ProductParser.php
@@ -23,4 +23,15 @@ class ProductParser {
     {
         return $this->crawler->filter('div.pdtInfos > strong > span')->text();
     }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        $price_txt = $this->crawler->filter('div.pdtInfos > div.pdtBuy > span.price')->text();
+        $price_txt = trim($price_txt);
+        $price_txt = str_replace(',', '.', $price_txt);
+        return (float)$price_txt;
+    }
 }
